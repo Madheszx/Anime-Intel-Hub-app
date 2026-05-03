ca84b23d0d0b6b910909f56727c2813db9f1a6cb
Madheszx <madeshvetri01@gmail.com>
2026-05-03 15:25:27 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.animeintelhub.app',
+  appName: 'Anime Intel Hub',
+  webDir: 'dist'
+};
+
+export default config;
